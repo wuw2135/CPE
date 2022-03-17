abd58b8bf0a57a1382eb23e5ce3f7d480254e342
wuw2135 <80589831+wuw2135@users.noreply.github.com>
2022-03-18 00:33:02 +0800

    .

---
+++ 一星題/Die game.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{	
+    int a,now,pop;
+    string b;
+    while(cin >> a,a){
+        deque<int> lfri = {1,4,6,3};
+        deque<int> updn = {1,2,6,5};
+        while(a--){
+            cin >> b;
+            if(b == "north"){
+                pop = updn.back();
+                updn.pop_back();
+                updn.push_front(pop);
+                now = pop;
+                lfri[0] = pop;
+                lfri[2] = updn[2];
+            }else if(b == "east"){
+                pop = lfri.back();
+                lfri.pop_back();
+                lfri.push_front(pop);
+                now = pop;
+                updn[0] = pop;
+                updn[2] = lfri[2];
+            }else if(b == "south"){
+                pop = updn.front();
+                updn.pop_front();
+                updn.push_back(pop);
+                now = updn.front();
+                lfri[0] = now;
+                lfri[2] = updn[2];
+            }else{
+                pop = lfri.front();
+                lfri.pop_front();
+                lfri.push_back(pop);
+                now = lfri.front();
+                updn[0] = now;
+                updn[2] = lfri[2];
+            }
+        }
+
+        cout << now << endl;
+    }
+
+}
---
+++ 一星題/Sort Sort and Sort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a,b;
+
+bool compare(int d,int e){
+    if((d%b) != (e%b)) return (d%b) < (e%b);
+    else{
+        if((d%2) * (e%2)) return d > e;
+        else if((d%2 == 0) && (e%2 == 0)) return d < e;
+        else return (d%2);
+    }
+}
+
+int main()
+{	
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int c;
+    vector<int> arr;
+    while(cin >> a >> b){
+        if(a == 0 & b == 0) {
+            cout << "0 0" << endl;
+            break;
+        }
+        arr.clear();
+        cout << a << " " << b << endl;
+        for(int i=0;i<a;i++){
+            cin >> c;
+            arr.push_back(c);
+        }
+
+        sort(arr.begin(),arr.end(),compare);
+
+        for(int f : arr) cout << f << endl;
+    }
+}
